5d4379c1ac2499ca731a847788f00793e92728e0
Vishalkiran Raichur <96622552+VishalkiranRaichur@users.noreply.github.com>
2026-03-28 13:02:05 -0700

    Add comments and improve code readability for customer, cart, and checkout modules
    - Added descriptive comments to Customer class
    - Improved readability of methods and constructors
    - Clarified purpose of key attributes and behaviors
    - Ensured consistency with other modules (cart and checkout)

--- src/customers/Customer.java
+++ src/customers/Customer.java
@@ -2,13 +2,16 @@ package customers;
 
 import cart.ShoppingCart;
 
+// Abstract base class for all customers in the grocery store system
 public abstract class Customer implements Displayable {
 
+    // Basic customer information
     protected int customerId;
     protected String firstName;
     protected String lastName;
     protected ShoppingCart cart;
 
+    // Constructor to create a customer with basic details
     public Customer(int customerId, String firstName, String lastName) {
         this.customerId = customerId;
         this.firstName = firstName;
@@ -16,14 +19,17 @@ public abstract class Customer implements Displayable {
         this.cart = new ShoppingCart();
     }
 
+    // Returns the customer ID
     public int getCustomerId() {
         return customerId;
     }
 
+    // Returns the customer's first name
     public String getFirstName() {
         return firstName;
     }
 
+    // Returns the customer's last name
     public String getLastName() {
         return lastName;
     }
@@ -33,6 +39,7 @@ public abstract class Customer implements Displayable {
         return firstName + " " + lastName;
     }
 
+    // Returns the customer's shopping cart
     public ShoppingCart getCart() {
         return cart;
     }
@@ -46,12 +53,14 @@ public abstract class Customer implements Displayable {
     System.out.println("No purchase history available yet.");
 }
 
+    // Displays customer information
     @Override
     public void displayCustomerInfo() {
         System.out.println("Customer ID: " + customerId);
         System.out.println("Name: " + firstName + " " + lastName);
     }
 
+    // Returns customer details as a string
     @Override
     public String toString() {
         return "Customer ID: " + customerId +
