3f579ad4d18fd7b3585c2162dc2ed3c8fee5937e
Vishalkiran Raichur <96622552+VishalkiranRaichur@users.noreply.github.com>
2026-03-28 13:11:31 -0700

    Add comments and improve code readability for customer, cart, and checkout modules
    Added descriptive comments to RegularCustomer

--- src/customers/RegularCustomer.java
+++ src/customers/RegularCustomer.java
@@ -1,11 +1,14 @@
 package customers;
 
+// Represents a regular customer in the system
 public class RegularCustomer extends Customer {
 
+    // Constructor for a regular customer
     public RegularCustomer(int customerId, String firstName, String lastName) {
         super(customerId, firstName, lastName);
     }
 
+    // Allows a regular customer to sign up for VIP membership
     public void signUpForVIP() {
         System.out.println(firstName + " " + lastName + " signed up for VIP membership.");
     }
